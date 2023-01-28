98f926690823986172bed58fda965ea073778547
BjornAM <bjorn.mansson@live.se>
2023-01-28 10:12:09 +0100

    added .env

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ dotenv.config()
 //
 
 // i framtiden: enviroment-variabler
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 1337
 
 app.listen(PORT, () => {
-  console.log('Server is listening on port' + 1337)
+  console.log('Server is listening on port' + PORT)
 })
