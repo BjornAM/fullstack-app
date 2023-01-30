7243dd02bdfd449163d215659706eb2318fa9173
BjornAM <bjorn.mansson@live.se>
2023-01-30 19:56:15 +0100

    input+button+lyssnare för channels

--- static/script.js
+++ static/script.js
@@ -1,17 +1,45 @@
+// Variables Channels
+const channelsContainer = document.querySelector(".channels")
+const inputChannel = document.querySelector(".input-channel")
+const btnAddChannel = document.querySelector("#btn-add-channel")
+const channelList = document.querySelector(".channel-list")
 
+//Variables users & password 
 const inputUsername = document.querySelector(".input-username");
 const inputPassword = document.querySelector(".input-password");
+const btnSignIn = document.querySelector("#btn-sign-in");
+const btnSignOut = document.querySelector("#btn-sign-out");
+const btnSignUp = document.querySelector("#btn-sign-up");
 
-const btnSignIn = document.querySelector("#sign-in-btn");
-const btnSignOut = document.querySelector("#sign-out-btn");
-const btnSignUp = document.querySelector("#sign-up-btn");
-
+//Variables messages
 const inputMessage = document.querySelector(".input-message");
-const btnSendMessage = document.querySelector("#send-btn");
+const btnSendMessage = document.querySelector("#btn-send-message");
 
-const channelsContainer = document.querySelector(".channels")
+let nextChannel = 1
 
-// Används med localStorage
+// Hämta data från backend
+
+
+// Event listeners
+
+btnAddChannel.addEventListener('click', () => {
+  const channelFromUser = inputChannel.value
+  const newChannel = {
+    id: nextChannel,
+    isDone: false,
+    description: channelFromUser
+  }
+  nextChannel++
+
+  const element = createTodoElement(newChannel)
+  channelList.appendChild(element)
+
+  data.push(newChannel)
+  //spara till backend
+
+})
+
+// Används med localStorage (samma för backend?)
 const JWT_KEY = "bookapi-jwt";
 let isLoggedIn = false;
 
